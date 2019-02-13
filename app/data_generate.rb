706debf846cf5ed2d1e56fcfa6a79da1400fbf3d
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-02-12 19:50:33 -0800

    Build data merging methods & fix run file name

--- app/data_generate.rb
+++ app/data_generate.rb
@@ -6,13 +6,29 @@ def get_all_songs_with_(header)
   https.use_ssl = true
 
   request = Net::HTTP::Get.new(url)
-  # request["with"] = "lyrics"
   request["with"] = header
 
   response = https.request(request)
 end
 
-def convert_songs_to_json
+def convert_songs_to_json(header)
   # take HTTPOK response and convert to json
-  JSON.parse(get_all_songs.body)
+  JSON.parse(get_all_songs_with_(header).body)
 end
+
+def flatten_artists(songs_array)
+  songs_array.each_with_object([]) do |song, arr|
+    song["artists"] = song["artists"].map { |artist| artist["name"] }.join(" & ")
+  end
+end
+
+def merge_lyrics_songs(songs, lyrics)
+  songs.each do |song|
+    song_lyrics = lyrics.select { |elmt| elmt["id"] == song["id"] }.first
+    song.merge(song_lyrics)
+  end
+end
+
+# raw_songs_artists = convert_songs_to_json("artists")
+# raw_songs_lyrics = convert_songs_to_json("lyrics")
+#
