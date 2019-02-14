e782a372b84dcdfefd6a3834d215c89802c967ef
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-02-14 10:49:27 -0800

    Update Song release_date column to data type date

--- db/migrate/20190213013754_create_songs.rb
+++ db/migrate/20190213013754_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.string :title
-      t.string :release_date
+      t.date :release_date
       t.text :lyrics
 
       t.timestamps
